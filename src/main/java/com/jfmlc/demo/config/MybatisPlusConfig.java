b80889b671c9a1460a87ecfca96890135f6351b5
jfm <2515651182@qq.com>
2018-11-22 14:25:25 +0800

    整合mybaits-plus

---
+++ src/main/java/com/jfmlc/demo/config/MybatisPlusConfig.java
@@ -0,0 +1,23 @@
+package com.jfmlc.demo.config;
+
+import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author fumei
+ * @date 2018-11-22 14:05
+ */
+@Configuration
+@MapperScan(basePackages = "com.jfmlc.demo.dao.mapper")
+public class MybatisPlusConfig {
+    /*
+     * 分页插件，自动识别数据库类型
+     * 多租户，请参考官网【插件扩展】
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
