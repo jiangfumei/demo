a1002b73635dfc68cbb3c8a069c36d078d528b18
jfm <2515651182@qq.com>
2018-11-23 13:55:49 +0800

    封装返回结果工具类

---
+++ src/main/java/com/jfmlc/demo/common/vo/Result.java
@@ -0,0 +1,41 @@
+package com.jfmlc.demo.common.vo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author fumei
+ * @date 2018-11-23 11:34
+ */
+@Data
+public class Result<T> implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 成功标志
+     */
+    private boolean success;
+
+    /**
+     * 失败消息
+     */
+    private String message;
+
+    /**
+     * 返回代码
+     */
+    private Integer code;
+
+    /**
+     * 时间戳
+     */
+    private long timestamp = System.currentTimeMillis();
+
+    /**
+     * 结果对象
+     */
+    private T result;
+
+}
